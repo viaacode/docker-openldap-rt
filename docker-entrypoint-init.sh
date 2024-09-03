b13fd05c768dcc4f0f4ac5e82871474f1853ae45
Herwig Bogaert <herwig.bogaert@meemoo.be>
2024-09-03 12:19:55 +0200

    refactor docker-entrypoint-init

--- docker-entrypoint-init.sh
+++ docker-entrypoint-init.sh
@@ -2,9 +2,19 @@
 # docker-entrypoint-init.sh
 # Process the ldif files in /docker-entrypoint-init
 
-# Start slapd without exposing it
+# Start slapd without exposing it during configuration
 # (listen only on internal unix domain socket)
-/usr/sbin/slapd -h ldapi:///
+coproc tailcop { exec /usr/sbin/slapd -d1 -h ldapi:/// 2>&1; }
+
+exec 3<&${tailcop[0]}
+
+while read -ru ${tailcop[0]} line; do
+    echo $line
+    [ $(expr "$line" : '.* slapd starting$') -gt 0 ] && break
+done
+
+echo "$(date '+%m/%d %H:%M:%S') ---- Configuring ldap ----"
+cat <&3 &
 
 # Process the ldif files
 for f in $(ls /docker-entrypoint-init/*ldif 2>/dev/null); do
@@ -15,6 +25,5 @@ for f in $(ls /docker-entrypoint-init/*ldif 2>/dev/null); do
     [ $? -eq 0 ] && mv $f $f.done
 done
 
-# Stop slapd
-pkill slapd
-while killall -s0 slapd 2>/dev/null; do sleep 1; done
+echo "$(date '+%m/%d %H:%M:%S') ---- Shutting down slapd ----"
+[ -n "$tailcop_PID" ] && kill $tailcop_PID && wait $tailcop_PID
